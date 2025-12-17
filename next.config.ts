b4d57f22250caabbbae85e9719e8a0a28936e70b
Roberto Costa <roberto.araujo339@gmail.com>
2025-12-17 18:29:17 -0300

    Add image configuration for TVMaze remote patterns and type declarations for CSS modules

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'static.tvmaze.com',
+      },
+    ],
+  },
 };
 
 export default nextConfig;
